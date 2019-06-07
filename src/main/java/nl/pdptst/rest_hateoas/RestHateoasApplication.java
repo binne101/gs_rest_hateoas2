7674537a046beaa906cbdcadbca47757a5a6b9d2
binne101 <binne101@gmail.com>
2019-06-07 20:32:40 +0200

    PDP-2: Test fail sonar

--- src/main/java/nl/pdptst/rest_hateoas/RestHateoasApplication.java
+++ src/main/java/nl/pdptst/rest_hateoas/RestHateoasApplication.java
@@ -2,6 +2,7 @@ package nl.pdptst.rest_hateoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.String;
 
 @SpringBootApplication
 public class RestHateoasApplication {
